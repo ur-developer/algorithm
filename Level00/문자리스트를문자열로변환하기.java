7abee5adb68e6073fff48cdbfada9e4f9da6b893
Park Min Ki <ur_developer92@naver.com>
2023-12-18 23:36:18 +0900

    코딩테스트 문제풀기

---
+++ Level00/문자리스트를문자열로변환하기.java
@@ -0,0 +1,12 @@
+package Level00;
+
+public class 문자리스트를문자열로변환하기 {
+	public String solution(String[] arr) {
+		String answer = "";
+
+			for(String a : arr) {
+	            answer += a;
+	        }
+        return answer;
+    }
+}

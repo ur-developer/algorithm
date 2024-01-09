6bc329ffc7306b570d59e350e66c105cd6a28b5b
Park Min Ki <ur_developer92@naver.com>
2024-01-09 23:53:16 +0900

    Codingtest practice

---
+++ CodingTestExam/SumofTwoNumbers.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class SumofTwoNumbers {
+	public static int calculatieSum(int a, int b) {
+		return a + b;
+	
+	}
+	
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+		int sum = calculatieSum(num1, num2);
+		System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum);
+	}
+}
